77ba389e1f0bde8160a5e7d26e46598794a4a41f
Eva Reitzel <evareitzel@gmail.com>
2022-07-08 15:38:25 -0400

    likes working ( + 1 ), click display event working (setup/delivery). git push

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const jokesContainer = document.getElementById('jokes-container');
 let jokeArray;
 
-fetch('http://localhost:3000/jokes/')
+function getJokes() {
+  fetch('http://localhost:3000/jokes/')
   .then(response => response.json())
   .then(function (data) {
     jokeArray = data;
+    // On initial render, send jokeArray to [ New Function - if/else for all/some jokes ]
     data.forEach((joke) => {
       renderJoke(joke);
+      // console.log(jokeArray);
     })
   })
+}
 
 // console.log(jokeArray);
 
@@ -25,7 +29,8 @@ function showJokes(jokeArray){
 }
 
 function addLike(event, joke, likes) {
-  const moreLikes = parseInt(joke.likes++);
+  event.preventDefault();
+  const moreLikes = parseInt(joke.likes + 1);
   fetch(`http://localhost:3000/jokes/${joke.id}`, {
     method: 'PATCH',
     headers: {
@@ -49,11 +54,9 @@ function renderJoke(joke) {
   const category = document.createElement('h3');
   category.innerText = joke.category;
 
-  const setup = document.createElement('p');
-  setup.innerText = joke.setup;
-
-  const delivery = document.createElement('p');
-  delivery.innerText = joke.delivery;
+  const setupDelivery = document.createElement('p');
+  setupDelivery.innerText = joke.setup;
+  setupDelivery.addEventListener('mouseover', (e) => displayDelivery(joke, e));
 
   const space = document.createElement('br');
 
@@ -67,59 +70,66 @@ function renderJoke(joke) {
   likes.className = 'likes';
   likes.innerText = (`${joke.likes} likes`);
 
-  card.append(category, setup, delivery, space, likeBtn, likes);
+  card.append(category, setupDelivery, space, likeBtn, likes);
 
   jokesContainer.append(card);
 }
 
+// card.addEventListener('click', changeColor) // mouseover
 
-const card = document.getElementsByClassName('card');
-// console.log(card);
-
-card.addEventListener('click', changeColor) // mouseover
-
-function changeColor() {
-  card.classList.toggle('card-delivery');
-}
-
-card.addEventListener('click', displayDelivery);
-
-function displayDelivery() {
-  const delivery = document.createElement('p');
-  delivery.innerText = joke.delivery;
-  card.append(delivery);
-}
-
-
-const searchQuery = document.getElementById('search');
-const submitBtn = document.getElementById('search-form');
-const results = document.getElementById('search-results');
-
-submitBtn.addEventListener('submit', fetchResults);
-
-function keywordSearch(joke) {
-  const searchResult = newJokeArray.filter(joke => joke.include(searchQuery));
-  console.log(searchResult);
-}
-
-const searchResult = newjokeArray.filter(containsSearchQuery);
-
-// function enhanceSearchQuery(){
-// modify searchQuery to include single, pluralized, non-capital letters
-  // toLowerCase(); // convert everything to lowercase
-  // toUpperCase();
-  // singular
-  // pluralize()
-
-
+// function changeColor() {
+//   card.classList.toggle('card-delivery');
 // }
 
-function containsSearchQuery(joke) {
-  joke.includes(searchQuery);
+// card.addEventListener('click', displayDelivery);
+
+function displayDelivery(joke, e) {
+  e.preventDefault();
+  e.target.innerText === joke.setup ? e.target.innerText = joke.delivery : e.target.innerText = joke.setup;
+
+  // const delivery = document.createElement('p');
+  // delivery.innerText = joke.delivery;
+  // card.append(delivery);
+  // e.target.innerText = joke.delivery;
+  //// console.log(joke, e);
 }
 
-  // jokes.append(card);
-  // append card to DOM
+const searchQuery = document.getElementById('search');
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', event => searchJokes(event));
 
-  // else - if empty? (default to full collection)
-  // If no matches, display 'No matches found"and display full joke list below.
+function searchJokes(event, searchQuery = "all") {
+  event.preventDefault();
+  console.log(event.target.value);
+  // #1. if searchQuery === all, render all jokes
+  // #2. else (iterate over filtered array and) render filtered jokes (search result).
+  // clear joke container btw renders.
+
+  // const searchResult = newJokeArray.filter(joke => joke.include(searchQuery));
+}
+
+// const searchResult = newjokeArray.filter(containsSearchQuery);
+
+// // function enhanceSearchQuery(){
+// // modify searchQuery to include single, pluralized, non-capital letters
+//   // toLowerCase(); // convert everything to lowercase
+//   // toUpperCase();
+//   // singular
+//   // pluralize()
+
+
+// // }
+
+// function containsSearchQuery(joke) {
+//   joke.includes(searchQuery);
+// }
+
+//   // jokes.append(card);
+//   // append card to DOM
+
+//   // else - if empty? (default to full collection)
+//   // If no matches, display 'No matches found"and display full joke list below.
+
+document.addEventListener("DOMContentLoaded", () => {
+  getJokes();
+})
